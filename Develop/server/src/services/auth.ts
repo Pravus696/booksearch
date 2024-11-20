6d1ced23264f233a619c31cdf55e16134b59a12a
Pravus696 <tbell3393@gmail.com>
2024-11-20 17:37:49 -0600

    auth.ts updated to include graphql

--- Develop/server/src/services/auth.ts
+++ Develop/server/src/services/auth.ts
@@ -10,13 +10,16 @@ interface JwtPayload {
   email: string,
 }
 
+const secretKey = process.env.JWT_SECRET_KEY || '';
+
+// Middleware function to authenticate token
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
   if (authHeader) {
     const token = authHeader.split(' ')[1];
 
-    const secretKey = process.env.JWT_SECRET_KEY || '';
+
 
     jwt.verify(token, secretKey, (err, user) => {
       if (err) {
@@ -31,6 +34,26 @@ export const authenticateToken = (req: Request, res: Response, next: NextFunctio
   }
 };
 
+// Auth middleware function for GraphQL
+export const authMiddleware = async ({ req }: { req: Request }) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
+
+    return jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        throw new Error('Forbidden'); // Forbidden
+      }
+
+      return user as JwtPayload;
+  });
+  } else {
+    throw new Error('Unauthorized'); // Unauthorized
+  }
+};
+
+// Function to sign token
 export const signToken = (username: string, email: string, _id: unknown) => {
   const payload = { username, email, _id };
   const secretKey = process.env.JWT_SECRET_KEY || '';
