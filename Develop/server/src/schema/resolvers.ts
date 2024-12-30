7382130b0bde7a4388662f34d1aa8efc9a0445df
Pravus696 <tbell3393@gmail.com>
2024-12-30 16:03:00 -0600

    npm install graphql

--- Develop/server/src/schema/resolvers.ts
+++ Develop/server/src/schema/resolvers.ts
@@ -1,4 +1,4 @@
-import { User } from '../models/index.js';
+import User from '../models/index.js';
 import { signToken, AuthenticationError } from '../services/auth.js';
 
 // To Do: Define types for the arguments
